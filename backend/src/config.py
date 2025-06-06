cd3b564ed4c8cdc6edc702f240a05e83979cd480
vladlen32230 <user@example.com>
2025-06-06 12:26:07 +0500

    add docker

--- backend/src/config.py
+++ backend/src/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENROUTER_API_KEY = os.environ["OPENROUTER_API_KEY"]
-MISTRAL_API_KEY = os.environ["MISTRAL_API_KEY"]
-DEEPINFRA_API_KEY = os.environ["DEEPINFRA_API_KEY"]
-DATABASE_URL = os.environ["DATABASE_URL"]
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+DEEPINFRA_API_KEY = os.getenv("DEEPINFRA_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
